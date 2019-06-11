1499fa6c2b3eb57a3ec19fa537e97631899845de
EugenieLABORDE <51073663+EugenieLABORDE@users.noreply.github.com>
2019-06-11 14:50:07 +0200

    Add files via upload

--- a rendre/pepper_lasers.py
+++ a rendre/pepper_lasers.py
@@ -14,12 +14,13 @@ def deplacement(X,Y,Theta):
 def main():
     simulation_manager = SimulationManager()
     client = simulation_manager.launchSimulation(gui=True)
-    coord =[-2, 5, 0]
-    pepper = simulation_manager.spawnPepper(client, spawn_ground_plane=False)
-    pepper2 = simulation_manager.spawnPepper(client, coord, spawn_ground_plane=False)
+    coord =[-2, 5, 0] #coordonnés de placement du deuxieme robot
+    pepper = simulation_manager.spawnPepper(client, spawn_ground_plane=False)#premier robot
+    pepper2 = simulation_manager.spawnPepper(client, coord, spawn_ground_plane=False)#deuxieme robot
     
     pybullet.setAdditionalSearchPath(pybullet_data.getDataPath())
 
+#changement du sol, mis en plus du parquet 
     pybullet.loadURDF(
         "plane.urdf",
         basePosition=[0, 0, 0],
@@ -28,14 +29,14 @@ def main():
 
     pybullet.loadURDF(
         "table.urdf",
-        basePosition=[-2, 0, 0],
+        basePosition=[-2, 0, 0],#coordonnés de placement de l'objet:(x,y,z)
         globalScaling=1,
        physicsClientId=client)
 
     pybullet.loadURDF(
         "bar.urdf",
         basePosition=[0, 5, 0.5],
-        baseOrientation=pybullet.getQuaternionFromEuler([1.57, 0, 0]),
+        baseOrientation=pybullet.getQuaternionFromEuler([1.57, 0, 0]),#rotation de 90° du bar selon l'axe x
         globalScaling=1,
        physicsClientId=client) 
 
@@ -61,13 +62,13 @@ def main():
 
     pybullet.loadURDF(
         "Cagetterouge.urdf",
-        basePosition=[2.4, -1, 0],
+        basePosition=[2.4, -1, 0],#cagette rouge placée derriere la cagette verte
         globalScaling=1,
         physicsClientId=client)
 
     pybullet.loadURDF(
        "Cagetteverte.urdf",
-        basePosition=[1.7, -1, 0],
+        basePosition=[1.7, -1, 0],#cagette verte placée derriere la cagette jaune
         globalScaling=1,
         physicsClientId=client)
 
@@ -77,28 +78,28 @@ def main():
         globalScaling=1,
         physicsClientId=client)
 
-    position=[-0.25,0.0,0.25]
-    i=random.choice(position) 
+    position=[-0.25,0.0,0.25]# trois positions possibles des objets
+    i=random.choice(position)#choix aléatoire des coordonnés selon y 
 
     pybullet.loadURDF(
      	   "totempizza.urdf",
-     	   basePosition=[-1.5, i, 1.0],
+     	   basePosition=[-1.5, i, 1.0],#position de l'objet avec coordonné selon y aléatoire
      	   globalScaling=1,
       	  physicsClientId=client)
-    del position[position.index(i)]
+    del position[position.index(i)]#suppression de la liste du coordonné de cet objet
 
     j=random.choice(position)
     pybullet.loadURDF(
         "totembanane.urdf",
-        basePosition=[-1.5, j, 1],
+        basePosition=[-1.5, j, 1],#position de l'objet avec coordonné selon y aléatoire
 	baseOrientation=pybullet.getQuaternionFromEuler([0, 0, 1.57]),
         globalScaling=1,
         physicsClientId=client)
-    del position[position.index(j)]
+    del position[position.index(j)]#suppression de la liste du coordonné de cet objet
 
     pybullet.loadURDF(
         "totemwine.urdf",
-        basePosition=[-1.5, position[0], 1],
+        basePosition=[-1.5, position[0], 1],#position de l'objet avec la dernier coordonnée selon y possible
         globalScaling=1,
         physicsClientId=client) 
    
